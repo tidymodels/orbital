207834c2ee2702edc694ac64e223a896e5ad2095
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-06-25 12:24:24 -0700

    fix bug where step_pca didn't work for 10+ columns because of wonky naming

--- R/step_pca.R
+++ R/step_pca.R
@@ -3,7 +3,8 @@ orbital.step_pca <- function(x, all_vars, ...) {
   rot <- x$res$rotation
   colnames(rot) <- recipes::names0(ncol(rot), x$prefix)
 
-  used_vars <- colnames(rot) %in% all_vars
+  used_vars <- pca_naming(colnames(rot), x$prefix) %in% 
+    pca_naming(all_vars, x$prefix)
 
   rot <- rot[, used_vars]
 
@@ -16,4 +17,8 @@ orbital.step_pca <- function(x, all_vars, ...) {
 
   names(out) <- all_vars
   out
+}
+
+pca_naming <- function(x, prefix) {
+  gsub(paste0(prefix, "0"), prefix, x)
 }
