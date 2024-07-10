4c8cb519e9650fea147e5410cc68752fe73caa6b
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-07-10 15:14:44 -0700

    test step_normalize() on spark

---
+++ tests/testthat/helper-utils.R
@@ -0,0 +1,13 @@
+## For sparklyr testing
+
+testthat_tbl <- function(name, data = NULL, repartition = 0L) {
+  sc <- testthat_spark_connection()
+
+  tbl <- tryCatch(dplyr::tbl(sc, name), error = identity)
+  if (inherits(tbl, "error")) {
+    if (is.null(data)) data <- eval(as.name(name), envir = parent.frame())
+    tbl <- dplyr::copy_to(sc, data, name = name, repartition = repartition)
+  }
+
+  tbl
+}
--- tests/testthat/test-step_normalize.R
+++ tests/testthat/test-step_normalize.R
@@ -14,3 +14,27 @@ test_that("step_normalize works", {
 
   expect_equal(res, exp)
 })
+
+test_that("spark - step_normalize works", {
+  skip_if_not_installed("recipes")
+  skip_if_not_installed("sparklyr")
+
+  mtcars <- dplyr::as_tibble(mtcars)
+
+  rec <- recipes::recipe(mpg ~ ., data = mtcars) %>%
+    recipes::step_normalize(recipes::all_predictors()) %>%
+    recipes::prep()
+
+  exp <- dplyr::mutate(mtcars, !!!orbital_inline(orbital(rec)))
+
+  sc <- testthat_spark_connection()
+  mtcars_tbl <- testthat_tbl("mtcars")
+
+  res_spark <- dplyr::mutate(mtcars_tbl, !!!orbital_inline(orbital(rec))) %>%
+    dplyr::collect()
+
+  expect_equal(res_spark, exp)
+})
+
+
+
