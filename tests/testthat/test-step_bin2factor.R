ebf2e5f5dd765b1860c084fc9cd746bc540a53de
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-07-19 13:38:05 -0700

    add step_bin2factor()

---
+++ tests/testthat/test-step_bin2factor.R
@@ -0,0 +1,43 @@
+test_that("step_bin2factor works", {
+  skip_if_not_installed("recipes")
+
+  mtcars <- dplyr::as_tibble(mtcars)
+
+  rec <- recipes::recipe(mpg ~ ., data = mtcars) %>%
+    recipes::step_bin2factor(vs, am) %>%
+    recipes::prep()
+
+  res <- dplyr::mutate(mtcars, !!!orbital_inline(orbital(rec)))
+
+  exp <- recipes::bake(rec, new_data = mtcars)
+  exp <- exp[names(res)]
+  exp$vs <- as.character(exp$vs)
+  exp$am <- as.character(exp$am)
+
+  expect_equal(res, exp)
+})
+
+test_that("spark - step_bin2factor works", {
+  skip_if_not_installed("recipes")
+  skip_if_not_installed("sparklyr")
+  skip_if(is.na(testthat_spark_env_version()))
+
+  mtcars <- dplyr::as_tibble(mtcars)
+
+  rec <- recipes::recipe(mpg ~ ., data = mtcars) %>%
+    recipes::step_bin2factor(vs, am) %>%
+    recipes::prep()
+
+  res <- dplyr::mutate(mtcars, !!!orbital_inline(orbital(rec)))
+
+  sc <- testthat_spark_connection()
+  mtcars_tbl <- testthat_tbl("mtcars")
+
+  res_spark <- dplyr::mutate(mtcars_tbl, !!!orbital_inline(orbital(rec))) %>%
+    dplyr::collect()
+
+  expect_equal(res_spark, res)
+})
+
+
+
