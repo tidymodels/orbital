5f2540d93e1dacbcc3732c0b72352a0b475a285a
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-07-20 21:06:02 -0700

    refactor step_pca

--- R/recipes-utils.R
+++ R/recipes-utils.R
@@ -19,3 +19,26 @@ lencode_helper <- function(x) {
   }
   out
 }
+
+pca_helper <- function(rot, prefix, all_vars) {
+  colnames(rot) <- recipes::names0(ncol(rot), prefix)
+
+  used_vars <- pca_naming(colnames(rot), prefix) %in% 
+    pca_naming(all_vars, prefix)
+
+  rot <- rot[, used_vars]
+
+  row_nms <- rownames(rot)
+
+  out <- character(length(all_vars))
+  for (i in seq_along(all_vars)) {
+    out[i] <- paste(row_nms, "*", rot[, i], collapse = " + ")
+  }
+
+  names(out) <- all_vars
+  out
+}
+
+pca_naming <- function(x, prefix) {
+  gsub(paste0(prefix, "0"), prefix, x)
+}
--- R/step_pca.R
+++ R/step_pca.R
@@ -1,24 +1,6 @@
 #' @export
 orbital.step_pca <- function(x, all_vars, ...) {
   rot <- x$res$rotation
-  colnames(rot) <- recipes::names0(ncol(rot), x$prefix)
-
-  used_vars <- pca_naming(colnames(rot), x$prefix) %in% 
-    pca_naming(all_vars, x$prefix)
-
-  rot <- rot[, used_vars]
-
-  row_nms <- rownames(rot)
-
-  out <- character(length(all_vars))
-  for (i in seq_along(all_vars)) {
-    out[i] <- paste(row_nms, "*", rot[, i], collapse = " + ")
-  }
-
-  names(out) <- all_vars
+  out <- pca_helper(rot, x$prefix, all_vars)
   out
-}
-
-pca_naming <- function(x, prefix) {
-  gsub(paste0(prefix, "0"), prefix, x)
 }
