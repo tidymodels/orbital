8b286491b4858e2912c42609dc600a55a2aa067a
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-07-20 18:17:26 -0700

    refactor out lencode section

---
+++ R/recipes-utils.R
@@ -0,0 +1,21 @@
+lencode_helper <- function(x) {
+  out <- character()
+
+  for (i in seq_along(x$mapping)) {
+    mapping <- x$mapping[[i]]
+    col <- names(x$mapping)[i]
+
+    new_ind <- mapping[["..level"]] == "..new"
+    levels <- mapping[["..level"]][!new_ind]
+    values <- mapping[["..value"]][!new_ind]
+    default <- mapping[["..value"]][new_ind]
+
+    eq <- paste0(col, " == \"", levels, "\" ~ ", values)
+    eq <- c(eq, paste0(".default = ", default))
+
+    eq <- paste0("dplyr::case_when(", paste0(eq, collapse = ", "), ")")
+    names(eq) <- col
+    out <- c(out, eq)
+  }
+  out
+}
--- R/step_lencode_bayes.R
+++ R/step_lencode_bayes.R
@@ -1,23 +1,5 @@
 #' @export
 orbital.step_lencode_bayes <- function(x, all_vars, ...) {
-  out <- character()
-
-  for (i in seq_along(x$mapping)) {
-    mapping <- x$mapping[[i]]
-    col <- names(x$mapping)[i]
-
-    new_ind <- mapping[["..level"]] == "..new"
-    levels <- mapping[["..level"]][!new_ind]
-    values <- mapping[["..value"]][!new_ind]
-    default <- mapping[["..value"]][new_ind]
-
-    eq <- paste0(col, " == \"", levels, "\" ~ ", values)
-    eq <- c(eq, paste0(".default = ", default))
-
-    eq <- paste0("dplyr::case_when(", paste0(eq, collapse = ", "), ")")
-    names(eq) <- col
-    out <- c(out, eq)
-  }
-  
+  out <- lencode_helper(x)
   out
 }
