4ef8dab7c6716a47422af948dce583a1abd8bb91
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-12-13 15:56:12 -0800

    air format

--- R/model-glm.R
+++ R/model-glm.R
@@ -1,17 +1,19 @@
 #' @export
 orbital.glm <- function(x, ..., mode = c("classification", "regression")) {
-  mode <- rlang::arg_match(mode)
+	mode <- rlang::arg_match(mode)
 
-  if (mode == "classification") {
-    levels <- levels(x$model$Species)
-    levels <- glue::double_quote(levels)
-    res <- tidypredict::tidypredict_fit(x)
-    res <- deparse1(res)
-    res <- glue::glue("dplyr::case_when({res} < 0.5 ~ {levels[1]}, .default = {levels[2]})")
-  }
+	if (mode == "classification") {
+		levels <- levels(x$model$Species)
+		levels <- glue::double_quote(levels)
+		res <- tidypredict::tidypredict_fit(x)
+		res <- deparse1(res)
+		res <- glue::glue(
+			"dplyr::case_when({res} < 0.5 ~ {levels[1]}, .default = {levels[2]})"
+		)
+	}
 
-  if (mode == "regression") {
-    res <- tidypredict::tidypredict_fit(x)
-  }
-  res
+	if (mode == "regression") {
+		res <- tidypredict::tidypredict_fit(x)
+	}
+	res
 }
--- R/parsnip.R
+++ R/parsnip.R
@@ -1,56 +1,56 @@
 #' @export
 orbital.model_fit <- function(x, ..., prefix = ".pred") {
-  mode <- x$spec$mode
+	mode <- x$spec$mode
 
-  check_mode(mode)
+	check_mode(mode)
 
-  res <- try(orbital(x$fit, mode = mode), silent = TRUE)
+	res <- try(orbital(x$fit, mode = mode), silent = TRUE)
 
-  if (inherits(res, "try-error")) {
-    res <- tryCatch(
-      tidypredict::tidypredict_fit(x),
-      error = function(cnd) {
-        if (grepl("no applicable method for", cnd$message)) {
-          cls <- class(x)
-          cls <- setdiff(cls, "model_fit")
-          cls <- gsub("^_", "", cls)
-  
-          cli::cli_abort(
-            "A model of class {.cls {cls}} is not supported.",
-            call = rlang::call2("orbital")
-          )
-        }
-        stop(cnd)
-      }
-    )
-  }
+	if (inherits(res, "try-error")) {
+		res <- tryCatch(
+			tidypredict::tidypredict_fit(x),
+			error = function(cnd) {
+				if (grepl("no applicable method for", cnd$message)) {
+					cls <- class(x)
+					cls <- setdiff(cls, "model_fit")
+					cls <- gsub("^_", "", cls)
 
-  if (mode == "classification") {
-    prefix <- paste0(prefix, "_class")
-  }
+					cli::cli_abort(
+						"A model of class {.cls {cls}} is not supported.",
+						call = rlang::call2("orbital")
+					)
+				}
+				stop(cnd)
+			}
+		)
+	}
 
-  if (is.language(res)) {
-    res <- deparse1(res)
-  }
+	if (mode == "classification") {
+		prefix <- paste0(prefix, "_class")
+	}
 
-  res <- stats::setNames(res, prefix)
-  
-  new_orbital_class(res)
+	if (is.language(res)) {
+		res <- deparse1(res)
+	}
+
+	res <- stats::setNames(res, prefix)
+
+	new_orbital_class(res)
 }
 
 #' @export
 orbital.model_spec <- function(x, ...) {
-  cli::cli_abort("{.arg x} must be fitted model.")
+	cli::cli_abort("{.arg x} must be fitted model.")
 }
 
 check_mode <- function(mode, call = rlang::caller_env()) {
-  supported_modes <- c("regression", "classification")
+	supported_modes <- c("regression", "classification")
 
-  if (!(mode %in% supported_modes)) {
-    cli::cli_abort(
-      "Only models with modes {.val {supported_modes}} are supported. 
+	if (!(mode %in% supported_modes)) {
+		cli::cli_abort(
+			"Only models with modes {.val {supported_modes}} are supported. 
       Not {.val {mode}}.",
-      call = call
-    )
-  }
+			call = call
+		)
+	}
 }
