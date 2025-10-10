7b1106ef7d9a9e6877e7a4108c8b4f6f4a6ea598
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-10-10 15:13:10 -0700

    add sqlite tailor tests

--- tests/testthat/test-adjust_predictions_custom.R
+++ tests/testthat/test-adjust_predictions_custom.R
@@ -90,3 +90,34 @@ test_that("spark - adjust_predictions_custom works", {
     predict(tlr_fit, mtcars)
   )
 })
+
+test_that("SQLite - adjust_predictions_custom works", {
+  skip_if_not_installed("tailor")
+  skip_if_not_installed("DBI")
+  skip_if_not_installed("RSQLite")
+  skip_on_cran()
+
+  tlr <- tailor::tailor() |>
+    tailor::adjust_predictions_custom(
+      double = mpg * 2,
+      half = mpg / 2
+    )
+
+  tlr_fit <- tailor::fit(
+    tlr,
+    mtcars,
+    outcome = c(mpg),
+    estimate = c(disp)
+  )
+
+  res <- orbital(tlr_fit)
+
+  con <- DBI::dbConnect(RSQLite::SQLite(), path = ":memory:")
+  mtcars_tbl <- dplyr::copy_to(con, mtcars)
+
+  expect_identical(
+    dplyr::collect(predict(res, mtcars_tbl)),
+    predict(tlr_fit, mtcars)
+  )
+  DBI::dbDisconnect(con)
+})
--- tests/testthat/test-adjust_probability_threshold.R
+++ tests/testthat/test-adjust_probability_threshold.R
@@ -137,3 +137,43 @@ test_that("spark - adjust_equivocal_zone works", {
     exp
   )
 })
+
+test_that("SQLite - adjust_equivocal_zone works", {
+  skip_if_not_installed("tailor")
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("workflows")
+  skip_if_not_installed("probably")
+  skip_if_not_installed("DBI")
+  skip_if_not_installed("RSQLite")
+  skip_on_cran()
+
+  mtcars$vs <- factor(mtcars$vs)
+
+  mod <- parsnip::logistic_reg()
+
+  workflow <- workflows::workflow()
+  workflow <- workflows::add_formula(workflow, vs ~ disp)
+  workflow <- workflows::add_model(workflow, mod)
+
+  tailor <- tailor::tailor()
+  tailor <- tailor::adjust_probability_threshold(
+    tailor,
+    threshold = 0.3
+  )
+  workflow <- workflows::add_tailor(workflow, tailor)
+
+  wf_fit <- workflows::fit(workflow, mtcars)
+
+  orb_fit <- orbital(wf_fit, type = c("prob", "class"))
+
+  exp <- as.character(predict(wf_fit, mtcars)$.pred_class)
+
+  con <- DBI::dbConnect(RSQLite::SQLite(), path = ":memory:")
+  mtcars_tbl <- dplyr::copy_to(con, mtcars)
+
+  expect_identical(
+    dplyr::pull(predict(orb_fit, mtcars_tbl), .pred_class),
+    exp
+  )
+  DBI::dbDisconnect(con)
+})
