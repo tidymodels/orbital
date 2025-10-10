555786f87c1d7382d28b5f6a082631367999349e
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-10-10 12:48:19 -0700

    add support for tailor::adjust_probability_threshold()

---
+++ tests/testthat/test-adjust_probability_threshold.R
@@ -0,0 +1,101 @@
+test_that("adjust_equivocal_zone works", {
+  skip_if_not_installed("tailor")
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("workflows")
+  skip_if_not_installed("probably")
+
+  mtcars$vs <- factor(mtcars$vs)
+
+  mod <- parsnip::logistic_reg()
+
+  workflow <- workflows::workflow()
+  workflow <- workflows::add_formula(workflow, vs ~ disp)
+  workflow <- workflows::add_model(workflow, mod)
+
+  tailor <- tailor::tailor()
+  tailor <- tailor::adjust_probability_threshold(
+    tailor,
+    threshold = 0.3
+  )
+  workflow <- workflows::add_tailor(workflow, tailor)
+
+  wf_fit <- workflows::fit(workflow, mtcars)
+
+  orb_fit <- orbital(wf_fit, type = c("prob", "class"))
+
+  exp <- as.character(predict(wf_fit, mtcars)$.pred_class)
+
+  expect_identical(
+    predict(orb_fit, mtcars)$.pred_class,
+    exp
+  )
+})
+
+test_that("adjust_equivocal_zone errors if types aren't set", {
+  skip_if_not_installed("tailor")
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("workflows")
+  skip_if_not_installed("probably")
+
+  mtcars$vs <- factor(mtcars$vs)
+
+  mod <- parsnip::logistic_reg()
+
+  workflow <- workflows::workflow()
+  workflow <- workflows::add_formula(workflow, vs ~ disp)
+  workflow <- workflows::add_model(workflow, mod)
+
+  tailor <- tailor::tailor()
+  tailor <- tailor::adjust_probability_threshold(tailor)
+  workflow <- workflows::add_tailor(workflow, tailor)
+
+  wf_fit <- workflows::fit(workflow, mtcars)
+
+  expect_snapshot(
+    error = TRUE,
+    orbital(wf_fit)
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    orbital(wf_fit, type = "prob")
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    orbital(wf_fit, type = "class")
+  )
+})
+
+test_that("adjust_equivocal_zone works with prefix argument", {
+  skip_if_not_installed("tailor")
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("workflows")
+  skip_if_not_installed("probably")
+
+  mtcars$vs <- factor(mtcars$vs)
+
+  mod <- parsnip::logistic_reg()
+
+  workflow <- workflows::workflow()
+  workflow <- workflows::add_formula(workflow, vs ~ disp)
+  workflow <- workflows::add_model(workflow, mod)
+
+  tailor <- tailor::tailor()
+  tailor <- tailor::adjust_probability_threshold(
+    tailor,
+    threshold = 0.3
+  )
+  workflow <- workflows::add_tailor(workflow, tailor)
+
+  wf_fit <- workflows::fit(workflow, mtcars)
+
+  orb_fit <- orbital(wf_fit, type = c("prob", "class"), prefix = "potato")
+
+  exp <- as.character(predict(wf_fit, mtcars)$.pred_class)
+
+  expect_identical(
+    predict(orb_fit, mtcars)$potato_class,
+    exp
+  )
+})
