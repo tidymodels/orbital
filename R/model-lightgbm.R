a692f3a991c2606c6021fabbbf7e8aa84c4b89f4
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-02-13 13:11:26 -0800

    add binary support for lightgbm

--- R/model-lightgbm.R
+++ R/model-lightgbm.R
@@ -4,7 +4,6 @@ orbital.lgb.Booster <- function(
   ...,
   mode = c("classification", "regression"),
   type = NULL,
-
   lvl = NULL
 ) {
   mode <- rlang::arg_match(mode)
@@ -13,7 +12,34 @@ orbital.lgb.Booster <- function(
   if (mode == "regression") {
     res <- tidypredict::tidypredict_fit(x)
   } else if (mode == "classification") {
-    cli::cli_abort("Classification mode not yet implemented for LightGBM.")
+    objective <- x$params$objective
+
+    if (objective %in% c("binary", "cross_entropy")) {
+      res <- lightgbm_binary(x, type, lvl)
+    } else {
+      cli::cli_abort(
+        "Multiclass classification not yet implemented for LightGBM."
+      )
+    }
+  }
+
+  res
+}
+
+lightgbm_binary <- function(x, type, lvl) {
+  eq <- tidypredict::tidypredict_fit(x)
+  eq <- deparse1(eq)
+
+  res <- NULL
+  if ("class" %in% type) {
+    levels <- glue::double_quote(lvl)
+
+    res <- c(
+      res,
+      orbital_tmp_class_name = glue::glue(
+        "dplyr::case_when({eq} > 0.5 ~ {levels[2]}, .default = {levels[1]})"
+      )
+    )
   }
 
   res
--- tests/testthat/test-model-lightgbm.R
+++ tests/testthat/test-model-lightgbm.R
@@ -34,3 +34,36 @@ test_that("boost_tree(engine = 'lightgbm'), objective = regression, works with t
     tolerance = 0.0000001
   )
 })
+
+test_that("boost_tree(engine = 'lightgbm'), objective = binary, works with type = class", {
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("bonsai")
+  skip_if_not_installed("tidypredict")
+  skip_if_not_installed("lightgbm")
+
+  mtcars$vs <- factor(mtcars$vs)
+
+  bt_spec <- parsnip::boost_tree(
+    mode = "classification",
+    engine = "lightgbm",
+    min_n = 1
+  )
+
+  bt_fit <- parsnip::fit(bt_spec, vs ~ disp + mpg + hp, mtcars)
+
+  orb_obj <- orbital(bt_fit, type = "class")
+
+  # to avoid exact split values
+  mtcars[, -8] <- mtcars[, -8] + 0.1
+
+  preds <- predict(orb_obj, mtcars)
+  exps <- predict(bt_fit, mtcars)
+
+  expect_named(preds, ".pred_class")
+  expect_type(preds$.pred_class, "character")
+
+  expect_identical(
+    preds$.pred_class,
+    as.character(exps$.pred_class)
+  )
+})
