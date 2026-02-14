29a9fc50df35e89d6c1163c49fcf056c16a1a50d
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-02-14 10:26:43 -0800

    make sure lightgbm works for class and prob in binary

--- tests/testthat/test-model-lightgbm.R
+++ tests/testthat/test-model-lightgbm.R
@@ -107,3 +107,48 @@ test_that("boost_tree(engine = 'lightgbm'), objective = binary, works with type 
     tolerance = 0.0000001
   )
 })
+
+test_that("boost_tree(engine = 'lightgbm'), objective = binary, works with type = c(class, prob)", {
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("bonsai")
+  skip_if_not_installed("tidypredict")
+  skip_if_not_installed("lightgbm")
+
+  mtcars$vs <- factor(mtcars$vs)
+
+  bt_spec <- parsnip::boost_tree(
+    mode = "classification",
+    engine = "lightgbm",
+    min_n = 1
+  )
+
+  bt_fit <- parsnip::fit(bt_spec, vs ~ disp + mpg + hp, mtcars)
+
+  orb_obj <- orbital(bt_fit, type = c("class", "prob"))
+
+  # to avoid exact split values
+  mtcars[, -8] <- mtcars[, -8] + 0.1
+
+  preds <- predict(orb_obj, mtcars)
+  exps <- dplyr::bind_cols(
+    predict(bt_fit, mtcars, type = "class"),
+    predict(bt_fit, mtcars, type = "prob")
+  )
+
+  expect_named(preds, c(".pred_class", ".pred_0", ".pred_1"))
+  expect_type(preds$.pred_class, "character")
+  expect_type(preds$.pred_0, "double")
+  expect_type(preds$.pred_1, "double")
+
+  exps <- as.data.frame(exps)
+  exps$.pred_class <- as.character(exps$.pred_class)
+
+  rownames(preds) <- NULL
+  rownames(exps) <- NULL
+
+  expect_equal(
+    preds,
+    exps,
+    tolerance = 0.0000001
+  )
+})
