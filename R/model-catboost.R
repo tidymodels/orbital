7ac6d8f35711f55ff79e081e6a9829a513066180
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-02-14 15:02:23 -0800

    add binary catboost support

--- R/model-catboost.R
+++ R/model-catboost.R
@@ -13,7 +13,34 @@ orbital.catboost.Model <- function(
   if (mode == "regression") {
     res <- tidypredict::tidypredict_fit(x)
   } else if (mode == "classification") {
-    res <- NULL
+    res <- catboost_binary(x, type, lvl)
+  }
+
+  res
+}
+
+catboost_binary <- function(x, type, lvl) {
+  eq <- tidypredict::tidypredict_fit(x)
+  eq <- deparse1(eq)
+
+  res <- NULL
+  if ("class" %in% type) {
+    levels <- glue::double_quote(lvl)
+
+    res <- c(
+      res,
+      orbital_tmp_class_name = glue::glue(
+        "dplyr::case_when({eq} > 0.5 ~ {levels[2]}, .default = {levels[1]})"
+      )
+    )
+  }
+  if ("prob" %in% type) {
+    # eq returns P(class 1), so P(class 0) = 1 - eq
+    res <- c(
+      res,
+      orbital_tmp_prob_name1 = glue::glue("1 - ({eq})"),
+      orbital_tmp_prob_name2 = glue::glue("{eq}")
+    )
   }
 
   res
--- tests/testthat/test-model-catboost.R
+++ tests/testthat/test-model-catboost.R
@@ -7,6 +7,7 @@ test_that("boost_tree(engine = 'catboost'), objective = regression, works with t
   bt_spec <- parsnip::boost_tree(
     mode = "regression",
     engine = "catboost",
+    trees = 50,
     min_n = 1
   )
 
@@ -24,6 +25,127 @@ test_that("boost_tree(engine = 'catboost'), objective = regression, works with t
   expect_type(preds$.pred, "double")
 
   exps <- as.data.frame(exps)
+
+  rownames(preds) <- NULL
+  rownames(exps) <- NULL
+
+  expect_equal(
+    preds,
+    exps,
+    tolerance = 0.0000001
+  )
+})
+
+test_that("boost_tree(engine = 'catboost'), objective = binary, works with type = class", {
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("bonsai")
+  skip_if_not_installed("tidypredict")
+  skip_if_not_installed("catboost")
+
+  mtcars$vs <- factor(mtcars$vs)
+
+  bt_spec <- parsnip::boost_tree(
+    mode = "classification",
+    engine = "catboost",
+    trees = 50,
+    min_n = 1
+  )
+
+  bt_fit <- parsnip::fit(bt_spec, vs ~ disp + mpg + hp, mtcars)
+
+  orb_obj <- orbital(bt_fit, type = "class")
+
+  # to avoid exact split values
+  mtcars[, -8] <- mtcars[, -8] + 0.01
+
+  preds <- predict(orb_obj, mtcars)
+  exps <- predict(bt_fit, mtcars)
+
+  expect_named(preds, ".pred_class")
+  expect_type(preds$.pred_class, "character")
+
+  expect_identical(
+    preds$.pred_class,
+    as.character(exps$.pred_class)
+  )
+})
+
+test_that("boost_tree(engine = 'catboost'), objective = binary, works with type = prob", {
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("bonsai")
+  skip_if_not_installed("tidypredict")
+  skip_if_not_installed("catboost")
+
+  mtcars$vs <- factor(mtcars$vs)
+
+  bt_spec <- parsnip::boost_tree(
+    mode = "classification",
+    engine = "catboost",
+    trees = 50,
+    min_n = 1
+  )
+
+  bt_fit <- parsnip::fit(bt_spec, vs ~ disp + mpg + hp, mtcars)
+
+  orb_obj <- orbital(bt_fit, type = "prob")
+
+  # to avoid exact split values
+  mtcars[, -8] <- mtcars[, -8] + 0.01
+
+  preds <- predict(orb_obj, mtcars)
+  exps <- predict(bt_fit, mtcars, type = "prob")
+
+  expect_named(preds, c(".pred_0", ".pred_1"))
+  expect_type(preds$.pred_0, "double")
+  expect_type(preds$.pred_1, "double")
+
+  exps <- as.data.frame(exps)
+
+  rownames(preds) <- NULL
+  rownames(exps) <- NULL
+
+  expect_equal(
+    preds,
+    exps,
+    tolerance = 0.0000001
+  )
+})
+
+test_that("boost_tree(engine = 'catboost'), objective = binary, works with type = c(class, prob)", {
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("bonsai")
+  skip_if_not_installed("tidypredict")
+  skip_if_not_installed("catboost")
+
+  mtcars$vs <- factor(mtcars$vs)
+
+  bt_spec <- parsnip::boost_tree(
+    mode = "classification",
+    engine = "catboost",
+    trees = 50,
+    min_n = 1
+  )
+
+  bt_fit <- parsnip::fit(bt_spec, vs ~ disp + mpg + hp, mtcars)
+
+  orb_obj <- orbital(bt_fit, type = c("class", "prob"))
+
+  # to avoid exact split values
+  mtcars[, -8] <- mtcars[, -8] + 0.01
+
+  preds <- predict(orb_obj, mtcars)
+  exps <- dplyr::bind_cols(
+    predict(bt_fit, mtcars, type = "class"),
+    predict(bt_fit, mtcars, type = "prob")
+  )
+
+  expect_named(preds, c(".pred_class", ".pred_0", ".pred_1"))
+  expect_type(preds$.pred_class, "character")
+  expect_type(preds$.pred_0, "double")
+  expect_type(preds$.pred_1, "double")
+
+  exps <- as.data.frame(exps)
+  exps$.pred_class <- as.character(exps$.pred_class)
 
   rownames(preds) <- NULL
   rownames(exps) <- NULL
