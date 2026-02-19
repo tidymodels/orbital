15f1149d359cc72caf20d03e0ff42110bbb382ce
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-02-19 13:28:50 -0800

    add earth multiclass support

--- R/model-earth.R
+++ R/model-earth.R
@@ -12,15 +12,18 @@ orbital.earth <- function(
   if (mode == "classification") {
     n_classes <- length(lvl)
     if (n_classes > 2) {
-      cli::cli_abort(
-        "Multiclass earth models are not yet supported."
-      )
-    }
-    # Binary classification - tidypredict_fit returns P(second level)
-    eq <- tidypredict::tidypredict_fit(x)
-    eq <- deparse1(eq)
+      # Multiclass classification
+      class_eqs <- tidypredict::.extract_earth_multiclass(x)
+      # Reorder to match lvl order
+      class_eqs <- class_eqs[lvl]
+      res <- multiclass_from_logits(unlist(class_eqs), type, lvl)
+    } else {
+      # Binary classification - tidypredict_fit returns P(second level)
+      eq <- tidypredict::tidypredict_fit(x)
+      eq <- deparse1(eq)
 
-    res <- binary_from_prob(eq, type, lvl)
+      res <- binary_from_prob(eq, type, lvl)
+    }
   } else if (mode == "regression") {
     res <- tidypredict::tidypredict_fit(x)
   }
--- tests/testthat/test-model-earth.R
+++ tests/testthat/test-model-earth.R
@@ -146,3 +146,142 @@ test_that("mars(engine = 'earth') works with custom prefix", {
 
   expect_named(preds, c("my_pred_class", "my_pred_0", "my_pred_1"))
 })
+
+# Multiclass earth tests
+# Note: These tests compare against raw earth predictions (fit$fit) rather than
+# parsnip predictions because parsnip has a bug with multiclass earth models
+# that returns incorrect probabilities and missing columns. The raw earth model
+# predictions are correct. See: https://github.com/tidymodels/parsnip/issues/1334
+
+test_that("mars(engine = 'earth') multiclass works with type = class", {
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("tidypredict")
+  skip_if_not_installed("earth")
+
+  spec <- parsnip::mars(mode = "classification", engine = "earth")
+
+  set.seed(123)
+  suppressWarnings(
+    fit <- parsnip::fit(spec, Species ~ ., iris)
+  )
+
+  orb_obj <- orbital(fit, type = "class")
+
+  preds <- predict(orb_obj, iris)
+
+  # Get expected classes from raw earth predictions
+  raw_probs <- predict(fit$fit, iris, type = "response")
+  exps_class <- colnames(raw_probs)[max.col(raw_probs, ties.method = "first")]
+
+  expect_named(preds, ".pred_class")
+  expect_type(preds$.pred_class, "character")
+
+  expect_identical(
+    preds$.pred_class,
+    exps_class
+  )
+})
+
+test_that("mars(engine = 'earth') multiclass works with type = prob", {
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("tidypredict")
+  skip_if_not_installed("earth")
+
+  spec <- parsnip::mars(mode = "classification", engine = "earth")
+
+  set.seed(123)
+  suppressWarnings(
+    fit <- parsnip::fit(spec, Species ~ ., iris)
+  )
+
+  orb_obj <- orbital(fit, type = "prob")
+
+  preds <- predict(orb_obj, iris)
+
+  # Get expected probabilities from raw earth predictions
+  raw_probs <- predict(fit$fit, iris, type = "response")
+  # Apply softmax to convert logits to probabilities
+  exp_probs <- exp(raw_probs)
+  exp_probs <- exp_probs / rowSums(exp_probs)
+  exps <- as.data.frame(exp_probs)
+  names(exps) <- paste0(".pred_", levels(iris$Species))
+
+  expect_named(preds, paste0(".pred_", levels(iris$Species)))
+  expect_type(preds$.pred_setosa, "double")
+  expect_type(preds$.pred_versicolor, "double")
+  expect_type(preds$.pred_virginica, "double")
+
+  rownames(preds) <- NULL
+  rownames(exps) <- NULL
+
+  expect_equal(
+    preds,
+    exps,
+    tolerance = 0.0000001
+  )
+})
+
+test_that("mars(engine = 'earth') multiclass works with type = c(class, prob)", {
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("tidypredict")
+  skip_if_not_installed("earth")
+
+  spec <- parsnip::mars(mode = "classification", engine = "earth")
+
+  set.seed(123)
+  suppressWarnings(
+    fit <- parsnip::fit(spec, Species ~ ., iris)
+  )
+
+  orb_obj <- orbital(fit, type = c("class", "prob"))
+
+  preds <- predict(orb_obj, iris)
+
+  # Get expected values from raw earth predictions
+  raw_probs <- predict(fit$fit, iris, type = "response")
+  exp_class <- colnames(raw_probs)[max.col(raw_probs, ties.method = "first")]
+  # Apply softmax to convert logits to probabilities
+  exp_probs <- exp(raw_probs)
+  exp_probs <- exp_probs / rowSums(exp_probs)
+
+  exps <- as.data.frame(exp_probs)
+  names(exps) <- paste0(".pred_", levels(iris$Species))
+  exps <- cbind(data.frame(.pred_class = exp_class), exps)
+
+  expect_named(preds, c(".pred_class", paste0(".pred_", levels(iris$Species))))
+  expect_type(preds$.pred_class, "character")
+  expect_type(preds$.pred_setosa, "double")
+  expect_type(preds$.pred_versicolor, "double")
+  expect_type(preds$.pred_virginica, "double")
+
+  rownames(preds) <- NULL
+  rownames(exps) <- NULL
+
+  expect_equal(
+    preds,
+    exps,
+    tolerance = 0.0000001
+  )
+})
+
+test_that("mars(engine = 'earth') multiclass works with custom prefix", {
+  skip_if_not_installed("parsnip")
+  skip_if_not_installed("tidypredict")
+  skip_if_not_installed("earth")
+
+  spec <- parsnip::mars(mode = "classification", engine = "earth")
+
+  set.seed(123)
+  suppressWarnings(
+    fit <- parsnip::fit(spec, Species ~ ., iris)
+  )
+
+  orb_obj <- orbital(fit, type = c("class", "prob"), prefix = "my_pred")
+
+  preds <- predict(orb_obj, iris)
+
+  expect_named(
+    preds,
+    c("my_pred_class", paste0("my_pred_", levels(iris$Species)))
+  )
+})
